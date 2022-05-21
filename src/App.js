023ec8f947ce46c3617334d1d617197353145dfd
handy-o <dyhan9420@gmail.com>
2022-05-21 11:30:00 +0900

    [useRef] 리얼돔객체 참조하여 state값 바꾸기

--- src/App.js
+++ src/App.js
@@ -1,36 +1,74 @@
 import './scss/style.scss'
-import { useState, useEffect } from 'react'
-import ColorBox from './ColorBox';
-import CountBox from './CountBox';
-import Popup from './Popup';
+import { useState, useRef, useEffect } from 'react'
+
 
 
 function App() {
-	console.log('app');
+	console.log('App function call again!')
+	let [num, setNum] = useState(0);
+	// state에 담아버리면?
+	//let [wid, setWid] = useState(null);
 
+	// useEffect(() => {
+	// 	const box = document.querySelector('.box'); -> 못찾음
+	// }, [])
 
+	const box = useRef(null);
+	const wid = useRef(null);
 	useEffect(() => {
-		console.log('처음 컴포넌트 생성')
-	}, []) // 콜백함수 + 의존성배열
+		window.addEventListener('resize', () => {
+			// 브라우저가 리사이즈 될 때마다 계속해서 화면을 재랜더링 
+			//setWid(window.innerWidth)
 
-	// 팝업 열고 닫기
-	const [pop, setPop] = useState(false);
+			// useEffect에 담아보면?
+			// 모션 실행시 자주 바뀌어야 되는 값을 담을때에도 useRef 사용
+			wid.current = window.innerWidth;
+			console.log(wid.current);
+		})
+	}, [])
+
+
+	// useEffect(() => {
+	// 	console.log(box)
+	// }, [])
+	useEffect(() => {
+		console.log(wid)
+	}, [wid])
 
 
 	return (
-		<>
-			<CountBox />
-			<ColorBox />
+		<section>
+			<button onClick={() => {
+				setNum(--num);
+				box.current.style.transform = `rotate(${45 * num}deg)`;
+			}}>prev</button>
+			<button onClick={() => {
+				setNum(++num);
+				box.current.style.transform = `rotate(${45 * num}deg)`;
+			}}>next</button>
 
-			<hr />
-			<hr />
-			<p>팝업이 안보이는 상태에서 버튼 클릭시 토글로 보였다 안보였다하게</p>
-			<button onClick={() => setPop(!pop)}> {pop ? '팝업 닫기' : '팝업 열기'} </button>
-
-			{pop ? <Popup /> : null}
-
-		</>
+			{/* 참조하고 싶은 가상돔에 ref 속성을 추가하고 useRef로 만든 빈 객체 연결 */}
+			<div className="box" ref={box}></div>
+		</section>
 	);
 }
 
 export default App;
+
+
+/*
+	useRef
+	아직 리얼돔으로 변경되지 않은 가상돔을 미리 객체에 담아서 참조
+
+	const 변수 = useRef(초기값);
+	- current 라는 키값이 있는 객체에 초기값을 담아서 변수에 할당
+	- 반환된 객체를 가상돔 요소에 ref속성으로 지정
+	- ref로 지정된 가상돔이 해당 객체에 참조 됨
+	- useRef는 보통 제어해야 되는 가상돔을 참조할 때 주로 쓰임
+	- useRef에 담겨있는 값이 변경이 될 때 화면이 재렌더링이 일어나지 않음
+	- 특정 컴포넌트 함수가 재렌더링 되더라도 useRef에 담겨있는 값은 계속 유지가 된다.
+	- useRef는 모션작업을 할때 특정 값이 모션에 의해서 게속 변경이 되더라도 해당 컴포넌트 함수를 재호출하지 않음
+	- ex)스크롤 할 때마다 지정된 박스의 위치로 스크롤해야할 때,, 스크롤할때마다 렌더링된다 ? 오.. 과부하
+
+
+*/
